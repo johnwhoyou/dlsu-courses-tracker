b963df63963ef37a0c8905dda21efc2628454d0c
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-05-08 13:50:21 +0800

    partially implemented SMS system

---
+++ sendSMSUpdate.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+
+const sendSMSUpdate = (phoneNumbers, message) => {
+  const url = "https://api.itexmo.com/api/broadcast";
+  const data = {
+    Email: "johncarlo.joyo@gmail.com",
+    Password: "p!n#eDV5yH4w@@bfYxPi",
+    Recipients: phoneNumbers,
+    Message: message,
+    ApiCode: "TR-JOHNC675612_USF3Y",
+    SenderId: "ITEXMO SMS",
+    PriorityLevel: "HIGH",
+  };
+
+  axios
+    .post(url, data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+sendSMSUpdate(
+  ["09177074814"],
+  "Hello world! Test message from johnwhoyou using ITEXMO API. atay ngano di man mugana"
+);
